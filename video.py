c76a7be2f06360f80fe29a4858cc3b7f3f34fab6
Skvalicharla <150321296+Skvalicharla@users.noreply.github.com>
2024-02-12 15:06:00 -0500

    video.py

--- video.py
+++ video.py
@@ -1,6 +1,6 @@
 import cv2
 
-vid = cv2.VideoCapture('DJI_0619_W.MP4')
+vid = cv2.VideoCapture('Video.MP4')
 num_frames = int(vid.get(cv2.CAP_PROP_FRAME_COUNT))
 
 for i in range(1, num_frames + 1):
@@ -9,9 +9,9 @@ for i in range(1, num_frames + 1):
         break
 
     if i % 10 == 0:
-        cv2.imwrite(f'Image_0619_{i}.jpg', frames)
+        cv2.imwrite(f'Image_0619_{i}.jpg', frames)  #Saves every 10th frame
 
-    #cv2.imwrite(f'Image_0359_{i}.jpg', frames)
+    #cv2.imwrite(f'Image_0359_{i}.jpg', frames)     #Saves every frame
 
 vid.release()
 cv2.destroyAllWindows()
